747e4cde66d04a5b6179c8975e6465e46afb1c61
abanuelo <32311654+abanuelo@users.noreply.github.com>
2018-11-16 00:30:44 -0800

    Squashed commit of the following:
    commit c6fd4cc438ed2c27247d45023c63f87ea69bfc26
    Author: Armando Bañuelos <32311654+abanuelo@users.noreply.github.com>
    Date:   Thu Nov 15 21:36:01 2018 -0800
    
        Create README.md

--- app/src/main/java/com/bignerdranch/android/pife11/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/pife11/MainActivity.java
@@ -2,6 +2,7 @@ package com.bignerdranch.android.pife11;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -18,15 +19,33 @@ import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
-    LoginButton loginButton;
-    TextView textView;
+    private LoginButton loginButton;
+    private TextView textView;
     public static CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Handles Transition to Create Account Page
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
+
+        /*
+            Method #1: If Users Select the Option to Register for a New Account
+        */
+        textView = (TextView) findViewById(R.id.createAccount);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCreateAccount();
+            }
+        });
+
+
+        /*
+            Method #2: Users opt selecting the option to login via Facebook
+         */
+
         loginButton = (LoginButton) findViewById(R.id.login_button);
         callbackManager = CallbackManager.Factory.create();
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
@@ -48,7 +67,67 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onActivityResult (int requestCode, int resultCode, Intent data){
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         callbackManager.onActivityResult(requestCode, resultCode, data);
     }
+
+    public void openCreateAccount() {
+        Intent intent = new Intent(this, CreateAccount.class);
+        startActivity(intent);
+    }
 }
+    //ATTEMPTING TO IMPLEMENT A GOOGLE ACCOUNT LOG-IN FOR USERS OF THE APP
+//    GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+////            .requestEmail()
+////            .build();
+////
+////    mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+////
+////    GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+////    updateUI(account);
+////
+////    @Override
+////    public void onClick(View v) {
+////        switch (v.getId()) {
+////            case R.id.sign_in_button:
+////                signIn();
+////                break;
+////            // ...
+////        }
+////    }
+////
+////    private void signIn() {
+////        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+////        startActivityForResult(signInIntent, RC_SIGN_IN);
+////    }
+////
+////    @Override
+////    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+////        super.onActivityResult(requestCode, resultCode, data);
+////
+////        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+////        if (requestCode == RC_SIGN_IN) {
+////            // The Task returned from this call is always completed, no need to attach
+////            // a listener.
+////            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+////            handleSignInResult(task);
+////        }
+////    }
+////
+////
+////    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+////        try {
+////            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+////
+////            // Signed in successfully, show authenticated UI.
+////            updateUI(account);
+////        } catch (ApiException e) {
+////            // The ApiException status code indicates the detailed failure reason.
+////            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+////            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+////            updateUI(null);
+////        }
+////    }
+
+
+
