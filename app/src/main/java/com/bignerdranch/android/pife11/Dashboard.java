9bd339069c4521bbd174d04de5a16d18d585a71d
Arkira Chantaratananond <arkira@stanford.edu>
2018-11-29 01:34:47 -0800

    draw avatar on homescreen

--- app/src/main/java/com/bignerdranch/android/pife11/Dashboard.java
+++ app/src/main/java/com/bignerdranch/android/pife11/Dashboard.java
@@ -1,11 +1,20 @@
 package com.bignerdranch.android.pife11;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Dashboard extends AppCompatActivity {
     private Button bPractice;
@@ -13,11 +22,18 @@ public class Dashboard extends AppCompatActivity {
     private Button bCollab;
     private ImageView rewardShop;
     private ImageView userProfile;
+    private String currentUserId;
+    private String myAvatar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        currentUserId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        getUserAvatar();
+
+
 
         bPractice = findViewById(R.id.practice);
         bCollab = findViewById(R.id.collab);
@@ -46,5 +62,31 @@ public class Dashboard extends AppCompatActivity {
                 startActivity(user_profile_intent);
             }
         });
+    }
+
+    private void getUserAvatar() {
+        DatabaseReference matchDb = FirebaseDatabase.getInstance().getReference().child("Users").child(currentUserId).child("Avatar");
+        matchDb.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()){
+                    myAvatar = dataSnapshot.getValue().toString().trim();
+                    ImageView avatarDisplay = (ImageView) findViewById(R.id.avatarDisplay);
+                    if(myAvatar.equals("{avatar=Jemi}")) {
+                        avatarDisplay.setImageResource(R.drawable.ic_monster_baby);
+                    } else {
+                        avatarDisplay.setImageResource(R.drawable.ic_nerdy_monster_baby);
+                    }
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
     }
 }
