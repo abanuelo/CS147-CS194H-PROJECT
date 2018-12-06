44a368cfe83e70d51cac001a0b80ee578ec16273
abanuelo <32311654+abanuelo@users.noreply.github.com>
2018-12-06 00:36:26 -0800

    Commit to master for the search performance

--- app/src/main/java/com/bignerdranch/android/pife11/watch_performance.java
+++ app/src/main/java/com/bignerdranch/android/pife11/watch_performance.java
@@ -1,13 +1,43 @@
 package com.bignerdranch.android.pife11;
 
+import android.net.Uri;
+import android.support.customtabs.CustomTabsIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class watch_performance extends AppCompatActivity {
+    private Button searchPerform1, searchPerform2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_watch_performance);
+
+        searchPerform1 = findViewById(R.id.button1);
+        searchPerform2 = findViewById(R.id.button2);
+
+        //Search Perform 2 for making it pop up forever
+        searchPerform1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://appear.in/piferoom1";
+                CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+                CustomTabsIntent customTabsIntent = builder.build();
+                customTabsIntent.launchUrl(watch_performance.this, Uri.parse(url));
+            }
+        });
+
+        //Search Perform 2 for making it pop up forever
+        searchPerform2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://appear.in/piferoom1";
+                CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+                CustomTabsIntent customTabsIntent = builder.build();
+                customTabsIntent.launchUrl(watch_performance.this, Uri.parse(url));
+            }
+        });
     }
 }
